bd6d78960d7181dae242a3a5aa70f52dd323a290
Harri Rautila <harri.rautila@gmail.com>
2012-12-09 18:33:47 +0200

    Fixed tests.

--- calgo/calgo_test.go
+++ calgo/calgo_test.go
@@ -2,17 +2,24 @@
 package calgo
 
 import (
-    "github.com/hrautila/matrix"
     "testing"
 )
 
 const NROWS = 2000
-var A, B, C *matrix.FloatMatrix
+var A, B, C []float64
 
 func TestMakeData(t *testing.T) {
-    A = matrix.FloatWithValue(NROWS, NROWS, 2.0)
-    B = matrix.FloatDiagonal(NROWS, 1.0)
-    C = matrix.FloatZeros(NROWS, NROWS)
+    // all values set matrix
+    A = make([]float64, NROWS*NROWS)
+    for i, _ := range A {
+        A[i] = 2.0
+    }
+    // diagonal matrix
+    B = make([]float64, NROWS*NROWS)
+    for i := 0; i < NROWS; i++ {
+        B[i*NROWS+i] = 1.0
+    }
+    C = make([]float64, NROWS*NROWS)
     t.Logf("A [%d,%d] non-zero matrix\n", NROWS, NROWS)
     t.Logf("B [%d,%d] diagonal matrix\n", NROWS, NROWS)
     t.Logf("C [%d,%d] result matrix\n", NROWS, NROWS)
@@ -20,11 +27,13 @@ func TestMakeData(t *testing.T) {
 }
 
 func TestMultAB(t *testing.T) {
-    Mult(0, C.Cols(), A.Rows(), B.Rows(), C.FloatArray(), A.FloatArray(), B.FloatArray())
+    // this should run quickly
+    Mult(C, A, B, 1.0, NROWS, NROWS, NROWS)
 }
 
 func TestMultBA(t *testing.T) {
-    Mult(0, C.Cols(), A.Rows(), B.Rows(), C.FloatArray(), B.FloatArray(), A.FloatArray())
+    // this take more time
+    Mult(C, B, A, 1.0, NROWS, NROWS, NROWS)
 }
 
 // Local Variables:
