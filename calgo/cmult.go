f7b385b503b0ee8b777ba902f93c2ec608e33670
Harri Rautila <harri.rautila@gmail.com>
2012-12-09 18:33:33 +0200

    Changed interfaces.

--- calgo/cmult.go
+++ calgo/cmult.go
@@ -8,23 +8,52 @@
 package calgo
 
 /*
-extern void mat_mult(int S, int N, int M, int P, double *C, double *A, double *B);
 
+extern void mult_vp_notrans(double *C, const double *A, const double *B,
+			    const double alpha,
+			    int M, int N, int P, int S, int L, int R, int E,
+			    int vlen);
 */
 // #cgo CFLAGS: -O3
 import "C"
 import "unsafe"
 
-
-// Calculate matrix-matrix product C = A*B for column major ordered matrices.
-// S is start column and N is end column in C, M is rows in A and P is rows in B
-// and columns in A. For full matrix product call with S=0, N=C.Cols.
-func Mult(S, N, M, P int, C, A, B []float64) {
-	C.mat_mult(C.int(S), C.int(N), C.int(M), C.int(P),
-		(*C.double)(unsafe.Pointer(&C[0])),
-		(*C.double)(unsafe.Pointer(&A[0])),
-		(*C.double)(unsafe.Pointer(&B[0])))
+// Calculate C +=alpha*A*B where C is M*N, A is M*P, B is P*N and alpha is scalar.
+// Arrays C, A, B are column major order matrix data arrays.
+func Mult(C, A, B []float64, alpha float64, M, N, P int) {
+    C.mult_vp_notrans(
+        (*C.double)(unsafe.Pointer(&C[0])),
+        (*C.double)(unsafe.Pointer(&A[0])),
+        (*C.double)(unsafe.Pointer(&B[0])), C.double(alpha),
+        C.int(M), C.int(N), C.int(P), C.int(0), C.int(N), C.int(0), C.int(M),
+        C.int(0))
 }
+
+
+// C += alpha*A*B for block defined by rows [R:E] and columns [S:L].
+// C is M*N, A is M*P, B is P*N and 0 <= L < S < N and 0 <= R < E < M
+func BlkMult(C, A, B []float64, alpha float64, M, N, P, S, L, R, E int) {
+    C.mult_vp_notrans(
+        (*C.double)(unsafe.Pointer(&C[0])),
+        (*C.double)(unsafe.Pointer(&A[0])),
+        (*C.double)(unsafe.Pointer(&B[0])), C.double(alpha),
+        C.int(M), C.int(N), C.int(P), C.int(S), C.int(L), C.int(R), C.int(E),
+        C.int(0))
+}
+
+// C += alpha*A*B for block defined by rows [R:E] and columns [S:L].
+// C is M*N, A is M*P, B is P*N and 0 <= L < S < N and 0 <= R < E < M
+// Panels in A and B are accessed in vlen blocks and accumulated to C.
+func BlkMultVp(C, A, B []float64, alpha float64, M, N, P, S, L, R, E, vlen int) {
+    C.mult_vp_notrans(
+        (*C.double)(unsafe.Pointer(&C[0])),
+        (*C.double)(unsafe.Pointer(&A[0])),
+        (*C.double)(unsafe.Pointer(&B[0])),
+        C.double(alpha),
+        C.int(M), C.int(N), C.int(P), C.int(S), C.int(L), C.int(R), C.int(E),
+        C.int(vlen))
+}
+
 
 // Local Variables:
 // tab-width: 4
